7d94eee7c9a891e122c0ce4a6ed7905ca9f4576a
sayali <sayalimoon34@gmail.com>
2026-03-23 21:11:41 +0530

    day1:basic API created

---
+++ main.py
@@ -0,0 +1,28 @@
+from fastapi import FastAPI
+from datetime import datetime
+
+app = FastAPI(
+    title="My First API",
+    description="Basic FastAPI project for learning",
+    version="1.0.0"
+)
+
+@app.get("/")
+def home():
+    return {
+        "message": "Welcome to my FastAPI project 🚀",
+        "status": "success"
+    }
+
+@app.get("/hello")
+def hello(name: str = "User"):
+    return {
+        "message": f"Hello, {name} 👋",
+        "time": datetime.now()
+    }
+
+@app.get("/health")
+def health_check():
+    return {
+        "status": "API is running perfectly ✅"
+    }
